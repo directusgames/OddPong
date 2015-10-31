210efbf17f5bbeb291bb28079d7b1dc3a204a483
John Iskra <john.m.iskra@gmail.com>
2015-10-31 17:06:55 +0800

    Added ball manager object which can spawn balls and remove all existing balls.

---
+++ OddPong/Assets/Scripts/BallManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class BallManager : MonoBehaviour
+{
+    public GameObject ball;
+
+    public void SpawnBall(Vector3 position, Vector3 startingVelocity)
+    {
+        Debug.Log("Spawning ball in position " + position.ToString() +
+            ", startingVelocity " + startingVelocity.ToString());
+
+        var spawned = (GameObject)Instantiate(ball);
+        spawned.tag = "Ball"; // TODO: This should be on the prefab.
+        var rigid = spawned.GetComponent<Rigidbody2D>();
+        if (rigid)
+        {
+            rigid.position = position;
+            rigid.velocity = startingVelocity;
+        }
+        else
+        {
+            Debug.LogWarning("Spawned ball but couldn't find rigidbody component.");
+        }
+    }
+
+    public void DeleteAllBalls()
+    {
+        var balls = GameObject.FindGameObjectsWithTag("Ball");
+        Debug.Log("Deleting " + balls.Length + " balls.");
+        foreach (var ball in balls)
+        {
+            Destroy(ball);
+        }
+    }
+}
