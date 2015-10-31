0928f475d77ebbc4e653e592110a6f5a4d5b5796
directusgames <directusgames@gmail.com>
2015-10-31 15:44:50 +0800

    Added ball movement

--- OddPong/Assets/Scripts/BallMovement.cs
+++ OddPong/Assets/Scripts/BallMovement.cs
@@ -8,12 +8,18 @@ public class BallMovement : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		
+		float rotation = Random.Range (0,360);		
+		Vector3 rotationVec = new Vector3(0,0,rotation);
+		transform.rotation = Quaternion.Euler(rotationVec);
+		gameObject.GetComponent<Rigidbody2D>().AddForce(transform.up * 5000);
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		//send off in random direction at set velocity
-		//
+	
+		
 		
 	}
 }
