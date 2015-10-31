ca7c9b258f5460d7e358fd4ea530b38a4272c54e
John Iskra <john.m.iskra@gmail.com>
2015-10-31 17:45:54 +0800

    Clean up line endings in Game script, and rename unhelpful "Text" to "Dotted Line".

--- OddPong/Assets/Scripts/Game.cs
+++ OddPong/Assets/Scripts/Game.cs
@@ -2,7 +2,8 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class Game : MonoBehaviour {
+public class Game : MonoBehaviour
+{
     public GameObject m_playerOne;
     public GameObject m_playerTwo;
 
@@ -18,8 +19,9 @@ public class Game : MonoBehaviour {
     public bool m_coolDown; // Whether in cool down state.
     public float m_cooldownTime; // Length of game cool down between rounds.
     private float m_cooldownStart; // Time cool down begun.
-    
-    void Start() {
+
+    void Start()
+    {
         p1Controller = m_playerOne.GetComponent<Player>();
         p2Controller = m_playerTwo.GetComponent<Player>();
         resetGame();
@@ -36,17 +38,23 @@ public class Game : MonoBehaviour {
         m_coolDown = false;
     }
 
-    public void playerScores(string player) {
+    public void playerScores(string player)
+    {
         Debug.Log("Player has scored: " + player);
         // Could be better.
-        if (player.Equals("PlayerOne")) {
+        if (player.Equals("PlayerOne"))
+        {
             p1Controller.incrementScore();
-        } else if (player.Equals("PlayerTwo")) {
+        }
+        else if (player.Equals("PlayerTwo"))
+        {
             p2Controller.incrementScore();
-        } else {
+        }
+        else
+        {
             Debug.LogError("Warning player scores with invalid string.");
         }
-        
+
         if (p1Controller.m_score >= m_maxScore || p2Controller.m_score >= m_maxScore)
         {
             m_winnerText.text = player + " wins!";
@@ -58,11 +66,14 @@ public class Game : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update() {
-        if (m_coolDown) {
-            if (System.Math.Abs(m_cooldownStart - Time.fixedTime) >= m_cooldownTime) {
+    void Update()
+    {
+        if (m_coolDown)
+        {
+            if (System.Math.Abs(m_cooldownStart - Time.fixedTime) >= m_cooldownTime)
+            {
                 resetGame();
             }
         }
-	}
+    }
 }
