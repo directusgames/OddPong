83300a6d32b4e57dd1609d517e6541e6fcdd0418
directusgames <directusgames@gmail.com>
2015-10-31 14:58:43 +0800

    Ball added
    Physics still required

---
+++ OddPong/Assets/BallMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallMovement : MonoBehaviour {
+	
+	public int speed;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey (KeyCode.D))
+		{
+			Vector3 pos = transform.position;
+			pos.x += speed;
+			transform.position = pos;
+		}
+	}
+}
