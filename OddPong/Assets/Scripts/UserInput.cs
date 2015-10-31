eca6a6a4aea9691d40a0604309583a62054567c7
Michael J. Kiernan <michaelk@pdonline.net.au>
2015-10-31 17:28:29 +0800

    Significant re-factor. Moved player properties to player class, moved collision detection to barriers.

---
+++ OddPong/Assets/Scripts/UserInput.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * Handle player 1 and player 2 input within the same script.
+ * Assumes the 2d camera has a 'size' of 200.
+ * 
+ * If there is a controller plugged in, it assumes this is for player 2.
+ * Unless we get time for UI/controller->player allocation.
+*/
+public class UserInput : MonoBehaviour
+{
+    public Rigidbody2D playerBody;
+    public float moveSpeed;
+    public bool arePlayerOne;
+
+    void Start()
+    {
+
+    }
+
+    // FixedUpdate is called once per physics update
+    void FixedUpdate()
+    {
+        float upDown = 0.0f;
+
+        // Player 1.
+        if (arePlayerOne)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                upDown = 1.0f;
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                upDown = -1.0f;
+            }
+        }
+        else
+        { // Player 2.
+            if (Input.GetKey(KeyCode.I))
+            {
+                upDown = 1.0f;
+            }
+            else if (Input.GetKey(KeyCode.K))
+            {
+                upDown = -1.0f;
+            }
+        }
+        playerBody.AddForce(new Vector3(0.0f, upDown, 0.0f) * moveSpeed);
+    }
+}
