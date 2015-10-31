72eb035da9a7501ec02944485cfbec9819bbbeb7
John Iskra <john.m.iskra@gmail.com>
2015-10-31 15:00:52 +0800

    Changed player movement to use velocity rather than setting position directly, and also reformatted the text file (Visual Studio defaults).

--- OddPong/Assets/Scripts/input.cs
+++ OddPong/Assets/Scripts/input.cs
@@ -8,32 +8,45 @@ using System.Collections;
  * If there is a controller plugged in, it assumes this is for player 2.
  * Unless we get time for UI/controller->player allocation.
 */
-public class input : MonoBehaviour {
+public class input : MonoBehaviour
+{
     public Rigidbody2D playerBody;
     public float moveSpeed = 0.5f;
     public bool playerOne;
 
-	void Start () {
-	
-	}
+    void Start()
+    {
+
+    }
 
     // Update is called once per frame
-    void Update() {
-        Vector3 curPos = playerBody.position;
+    void Update()
+    {
+        Vector3 velocity = new Vector3(0.0f, 0.0f, 0.0f);
 
         // Player 1.
-        if (playerOne) {
-            if (Input.GetKey(KeyCode.W)) {
-                playerBody.MovePosition(new Vector2(curPos.x, curPos.y + moveSpeed * Time.fixedDeltaTime));
-            } else if (Input.GetKey(KeyCode.S)) {
-                playerBody.MovePosition(new Vector2(curPos.x, curPos.y - moveSpeed * Time.fixedDeltaTime));
+        if (playerOne)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                velocity.y = moveSpeed;
             }
-        } else { // Player 2.
-            if (Input.GetKey(KeyCode.I)) {
-                playerBody.MovePosition(new Vector2(curPos.x, curPos.y + moveSpeed * Time.fixedDeltaTime));
-            } else if (Input.GetKey(KeyCode.K)) {
-                playerBody.MovePosition(new Vector2(curPos.x, curPos.y - moveSpeed * Time.fixedDeltaTime));
+            else if (Input.GetKey(KeyCode.S))
+            {
+                velocity.y = -moveSpeed;
             }
         }
+        else
+        { // Player 2.
+            if (Input.GetKey(KeyCode.I))
+            {
+                velocity.y = moveSpeed;
+            }
+            else if (Input.GetKey(KeyCode.K))
+            {
+                velocity.y = -moveSpeed;
+            }
+        }
+        playerBody.velocity = velocity;
     }
 }
