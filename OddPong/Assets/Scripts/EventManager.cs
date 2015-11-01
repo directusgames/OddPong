333e3cd72bcaa2440fa84a4a3e4d30c55b112c2d
Michael J. Kiernan <mikeyjkiernan@gmail.com>
2015-11-01 16:08:36 +0800

    Only checking timing for swapping player event (making way for event manager).

---
+++ OddPong/Assets/Scripts/EventManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventManager : MonoBehaviour {
+
+    GravityManip m_gravityManipulator;
+
+    public bool m_triggerGravity;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (m_triggerGravity)
+        {
+            m_gravityManipulator.PlayerGravity();
+            m_triggerGravity = false;
+        }
+	}
+}
--- OddPong/Assets/Scripts/GravityManip.cs
+++ OddPong/Assets/Scripts/GravityManip.cs
@@ -1,10 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-// Allow normal play for 1 minute.
-// Swap to strong left for 4 s.
-// Swap to strong right for 4 s.
-// Return to normal play (ad infinitum).
 public class GravityManip : MonoBehaviour {
     public Vector2 m_gravity;
 
@@ -15,59 +11,53 @@ public class GravityManip : MonoBehaviour {
     private Vector2 m_strongLeft = new Vector2(-40, 0);
     private Vector2 m_strongRight = new Vector2(40, 0);
 
-    public float m_roundTime = 60; // Round time in seconds.
-    public float m_intervalTime = 4; // Gravity interval in seconds.
-
-    public bool m_inRound; // Currently in normal play.
     public bool m_swapPlayer;
+    public float m_intervalTime; // Gravity interval in seconds.
     public float m_prevTime; // Start time to measure against current time.
 
     // WIP / broken:
     // private Vector2 slimeSoccerUp = new Vector2(0, 10);
     // private Vector2 slimeSoccerDown = new Vector2(0, -10);
 
-    void Start () {
+    public void NormalGravity()
+    {
+        m_ballManager.AntiGravBalls();
+    }
+
+    private void SwapPlayer()
+    {
+        m_swapPlayer = false;
+        // Invert vector function instead?
+        m_gravity = m_gravity.Equals(m_strongLeft) ? m_strongRight : m_strongLeft;
         m_prevTime = Time.fixedTime;
-        m_inRound = true;
+        Physics2D.gravity = m_gravity;
+    }
+
+    public void PlayerGravity()
+    {
+        m_ballManager.GravBalls();
+        m_swapPlayer = true; // Next iteration we will want to swap the player.
+
+        // Randomise the pain.
+        int rand = Random.Range(0, 1);
+        m_gravity = rand == 1 ? m_strongLeft : m_strongRight;
+        m_prevTime = Time.fixedTime;
+        Physics2D.gravity = m_gravity;
+    }
+
+    void Start()
+    {
         m_swapPlayer = false;
     }
-	
-	void Update () {
-        if (m_inRound)
+
+    void Update()
+    {
+        if (m_swapPlayer)
         {
-            if (System.Math.Abs(m_prevTime - Time.fixedTime) >= m_roundTime)
+            if ((System.Math.Abs(m_prevTime - Time.fixedTime)) >= m_intervalTime)
             {
-                Debug.Log("normal grav -> random player grav");
-                m_ballManager.GravBalls();
-                m_inRound = false; // No longer normal gravity.
-                m_swapPlayer = true; // Next iteration we will want to swap the player.
-
-                // Randomise the pain.
-                int rand = Random.Range(0, 1);
-                m_gravity = rand == 1 ? m_strongLeft : m_strongRight;
-                m_prevTime = Time.fixedTime;
-                Physics2D.gravity = m_gravity;
-            }
-        } else if ((System.Math.Abs(m_prevTime - Time.fixedTime)) >= m_intervalTime) {
-            if (m_swapPlayer) // Invert grav.
-            {
-                Debug.Log("random player -> opposite player grav");
-                m_swapPlayer = false;
-                // Invert vector function instead?
-                m_gravity = m_gravity.Equals(m_strongLeft) ? m_strongRight : m_strongLeft;
-                m_prevTime = Time.fixedTime;
-                Physics2D.gravity = m_gravity;
-
-
-            } else // Back to normal play.
-            {
-                Debug.Log("opposite -> normal grav");
-                m_inRound = true;
-                m_prevTime = Time.fixedTime;
-
-                m_ballManager.AntiGravBalls();
+                SwapPlayer();
             }
         }
-        
     }
 }
