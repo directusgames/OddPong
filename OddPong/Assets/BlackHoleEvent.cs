2e246d8f1f4df77e92f6a65ba1942e9b2522b187
directusgames <directusgames@gmail.com>
2015-11-01 20:29:55 +0800

    Altered event manager to include black hole

---
+++ OddPong/Assets/BlackHoleEvent.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlackHoleEvent : MonoBehaviour, GameEvent {
+
+	public GameObject blackHole;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public void StartRandomEvent()
+	{
+		float camHeight = Camera.main.orthographicSize / 2.0f;
+		Vector3 spawnPos = new Vector3(0, Random.Range (camHeight, -camHeight), 0);
+		
+		Instantiate(blackHole, spawnPos, Quaternion.identity);
+	}
+	
+	public void StopRandomEvent()
+	{	
+		
+	}
+}
