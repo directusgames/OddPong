b5a331a055bae3ce8c574e0018f1974a73e5307c
directusgames <directusgames@gmail.com>
2015-11-01 18:28:46 +0800

    Initial work on gravity effect. working on fade speed.

---
+++ OddPong/Assets/GravityEffectController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravityEffectController : MonoBehaviour {
+
+	public int fadeSpeed;
+	public int alphaVal;
+	
+	private int currentAlpha;
+
+
+	// Use this for initialization
+	void Start () {
+	
+		currentAlpha = 0;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void FadeIn()
+	{
+		
+		if(currentAlpha <= alphaVal)
+		{
+			currentAlpha = currentAlpha + fadeSpeed;
+			newAlpha.a = currentAlpha;
+			GetComponent<Renderer>().material.color.a = newAlpha;
+		}
+	}
+	
+	void FadeOut()
+	{
+	}
+}
